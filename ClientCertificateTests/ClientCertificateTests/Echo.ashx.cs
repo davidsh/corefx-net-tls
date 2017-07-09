488deb044977e8a72ce7c18d82fa50ff4fbe2d0d
David Shulman <david.shulman@microsoft.com>
2017-07-09 08:05:48 -0700

    Add Echo endpoint

---
+++ ClientCertificateTests/ClientCertificateTests/Echo.ashx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ClientCertificateTests
+{
+    /// <summary>
+    /// Summary description for Echo
+    /// </summary>
+    public class Echo : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var headers = context.Request.Headers;
+
+            var buffer = new StringBuilder();
+            foreach (string key in headers.Keys)
+            {
+                buffer.Append($"{key}: ");
+                foreach (var value in headers.GetValues(key))
+                {
+                    buffer.Append($"{value} ");
+                }
+                buffer.AppendLine();
+            }
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(buffer.ToString());
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
